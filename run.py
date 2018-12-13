3001767b1b6a45c28586b4b23ea20a1aed378302
DLangerr <33069450+DLangerr@users.noreply.github.com>
2018-12-13 11:40:21 +0100

    Add files via upload

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
-from bfinterpreter import brainfuck_to_py, get_output
+from bfinterpreter import BrainfuckInterpreter
 from tkinter import *
 class GUI():
 	def __init__(self, master):
+		self.bfi = BrainfuckInterpreter()
 		self.master = master
 		self.master.minsize(width=700, height=450)
 		self.master.geometry('800x550')
-		master.title("Brainfucker interpreter")
+		master.title("Brainfuck interpreter")
 
 		self.code_label = Label(master, text="Code")
 		self.code_label.pack()
@@ -32,19 +33,19 @@ class GUI():
 		self.close_button = Button(master, text="Close", command=master.quit)
 		self.close_button.pack()
 
-
-
 	def run(self):
 		code = self.code_entry.get("1.0",'end-1c')
 		text = self.text_entry.get("1.0",'end-1c')
-		brainfuck_to_py(code)
-		self.set_text_out(get_output(text))
+		self.bfi.brainfuck_to_py(code)
+		self.set_text_out(self.bfi.get_output(text))
 
 	def set_text_out(self, text):
 		self.text_out.configure(state="normal")
 		self.text_out.delete('1.0', END)
 		self.text_out.insert(INSERT, text)
 		self.text_out.configure(state="disabled")
+
+
 
 root = Tk()
 root.iconbitmap("dl.ico")
